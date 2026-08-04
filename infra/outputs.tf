632a1063af963d28955e74e638a92522a4e94410
TRP <t@trp.dev>
2026-08-04 14:48:37 -0600

    remove terraform

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "registry_name" {
-  description = "DigitalOcean container registry name"
-  value       = digitalocean_container_registry.this.name
-}
-
-output "registry_endpoint" {
-  description = "Registry endpoint used for image tags"
-  value       = "registry.digitalocean.com/${digitalocean_container_registry.this.name}"
-}
